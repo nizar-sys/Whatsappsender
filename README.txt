573bdb51d8ae6ff7bfaf9b31168bd9dce9100a85
nizar-sys <74032907+nizar-sys@users.noreply.github.com>
2021-02-09 11:41:11 +0700

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,27 @@
+==================================
+Pengiriman pesan via python
+==================================
+
+Cara menggunakan:
+ - Edit kontak.txt dengan format 
+	tujuan;Receiver 1
+	untuk tujuan, bisa hanya 4 nomor belakang
+	contoh : - 1853;Receiver 1
+- ketik python bot.py di terminal atau git bash
+
+untuk proram ini membutuhkan:
+
+1. Selenium
+	Cara install selenium :
+		pip install selenium
+2. Chromedriver
+3  Chrome browser
+
+Kebutuhan diatas sudah ada di folder ini tenang aja:D
+
+#anakmudapakesanca
+
+
+Follow me on :
+	IG = @niizar.id
+	Github = nizar-sys 
---
+++ bot.py
@@ -0,0 +1,54 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.keys import Keys
+import csv
+
+
+def element_presence(by, xpath, time):
+    element_present = EC.presence_of_element_located((By.XPATH, xpath))
+    WebDriverWait(driver, time).until(element_present)
+
+
+def send_message(receiver, message):
+    element_presence(
+        By.XPATH, '//*[@id="side"]/div[1]/div/label/div/div[2]', 30)
+    receiver_element = driver.find_element(
+        By.XPATH, '//*[@id="side"]/div[1]/div/label/div/div[2]')
+    receiver_element.send_keys(receiver)
+    receiver_element.send_keys("\n")
+    element_presence(
+        By.XPATH, '//*[@id="main"]/footer/div[1]/div[2]/div/div[2]', 30)
+    msg_box = driver.find_element(
+        By.XPATH, '//*[@id="main"]/footer/div[1]/div[2]/div/div[2]')
+    msg_box.send_keys(message)
+    msg_box.send_keys("\n")
+
+
+chrome_options = Options()
+chrome_options.add_argument("--user-data-dir=D:/2020 python/selepy/Selenium")
+chrome_options.add_argument('--profile-directory=Default')
+driver = webdriver.Chrome(
+    executable_path='chromedriver/chromedriver.exe', options=chrome_options)
+driver.get("https://web.whatsapp.com/")
+
+# masukkan pesan kamu di sini
+message = "List Absensi Kehadiran Pagi :\ue008\ue007muhamad nizar"
+message = str.lower(message)
+# textarea = driver.find_element_by_tag_name("div")
+
+# if '\r\n' in message:
+#     messagesplit = message.split("\r\n")
+#     messagesplit_len = len(messagesplit)-1
+#     for message in messagesplit:
+#         textarea.send_keys(message)
+#         if messagesplit.index(message) != messagesplit_len:
+#             textarea.send_keys('\ue008' + '\ue007')
+
+with open('kontak.txt') as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter=";")
+    for row in csv_reader:
+        send_message(row[0], message)
